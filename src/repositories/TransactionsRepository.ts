36c84c22d4b3125500d0960e536d41b029c60314
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2020-04-15 19:43:51 -0300

    feat: create transactionsRepository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,61 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  private getIncomeValue(): number {
+    return this.transactions.reduce((total, transaction) => {
+      if (transaction.type === 'income') return total + transaction.value;
+
+      return total;
+    }, 0);
+  }
+
+  private getOutcomeValue(): number {
+    return this.transactions.reduce((total, transaction) => {
+      if (transaction.type === 'outcome') return total + transaction.value;
+
+      return total;
+    }, 0);
   }
 
   public getBalance(): Balance {
-    // TODO
+    this.balance.income = this.getIncomeValue();
+    this.balance.outcome = this.getOutcomeValue();
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
